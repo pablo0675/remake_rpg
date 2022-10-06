39b3a6fe12ffa705ea3726136b3d57e92dd9cea2
Pablo Levy <pablo.levy@epitech.eu>
2022-10-06 15:02:28 +0200

    :baguette_bread:  fill flags entries

--- src/get_arguments.c
+++ src/get_arguments.c
@@ -1,4 +1,4 @@
-#include "my_rpg.h"
+#include "../includes/my_rpg.h"
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,5 +6,21 @@
 
 void get_arguments(game_t *game, int ac, char **av)
 {
+    game->flag->nb_players = 0;
+    game->flag->game_difficulty = 0;
+    game->flag->map_size[0] = 0;
+    game->flag->map_size[1] = 0;
+    game->flag->help = 0;
+
+    for (int i = 0; i < ac; i++) {
+        if (av[i][0] == '-' && av[i][1] == 'h')
+            game->flag->help = 1;
+        if (av[i][0] == '-' && av[i][1] == 'p' && isdigit(av[i][2]))
+            game->flag->nb_players = atoi(av[i] + 2);
+        if (av[i][0] == '-' && av[i][1] == 'd' && isdigit(av[i][2]))
+            game->flag->game_difficulty = atoi(av[i] + 2);
+        if (av[i][0] == '-' && av[i][1] == 'm' && isdigit(av[i][2]))
+            game->flag->map_size[0] = atoi(av[i] + 2);
+    }
     return;
 }
