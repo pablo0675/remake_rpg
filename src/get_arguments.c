f89cfe2e3a1472362d6c970c8632f37c03ff36b0
Pablo Levy <pablo.levy@epitech.eu>
2022-10-06 17:13:06 +0200

    :sparkle: adjust flag add print usage :sparkle:

--- src/get_arguments.c
+++ src/get_arguments.c
@@ -6,21 +6,22 @@
 
 void get_arguments(game_t *game, int ac, char **av)
 {
-    game->flag->nb_players = 0;
-    game->flag->game_difficulty = 0;
-    game->flag->map_size[0] = 0;
-    game->flag->map_size[1] = 0;
+    game->flag->nb_players = 1;
+    game->flag->game_difficulty = 1;
+    game->flag->map_size[0] = 10;
+    game->flag->map_size[1] = 10;
     game->flag->help = 0;
 
     for (int i = 0; i < ac; i++) {
         if (av[i][0] == '-' && av[i][1] == 'h')
             game->flag->help = 1;
-        if (av[i][0] == '-' && av[i][1] == 'p' && isdigit(av[i][2]))
-            game->flag->nb_players = atoi(av[i] + 2);
-        if (av[i][0] == '-' && av[i][1] == 'd' && isdigit(av[i][2]))
-            game->flag->game_difficulty = atoi(av[i] + 2);
-        if (av[i][0] == '-' && av[i][1] == 'm' && isdigit(av[i][2]))
-            game->flag->map_size[0] = atoi(av[i] + 2);
+        if (av[i][0] == '-' && av[i][1] == 'p' && isdigit(av[i + 1][0]))
+            game->flag->nb_players = atoi(av[i + 1]);
+        if (av[i][0] == '-' && av[i][1] == 'd' && isdigit(av[i + 1][0]))
+            game->flag->game_difficulty = atoi(av[i + 1]);
+        if (av[i][0] == '-' && av[i][1] == 'm' && (isdigit(av[i + 1][0]) && isdigit(av[i + 2][0]))) {
+            game->flag->map_size[0] = atoi(av[i + 1]);
+            game->flag->map_size[1] = atoi(av[i + 2]);
+        }
     }
-    return;
 }
--- src/main.c
+++ src/main.c
@@ -1,8 +1,43 @@
 #include "my_rpg.h"
 #include <unistd.h>
-#include <malloc.h>
-#include "printf.h"
+#include "my_lib.h"
 #include "struct.h"
+
+void print_usage()
+{
+    my_printf("%sUSAGE:\n"
+              "-h                   :      help, print the usage and quit "
+              "the program\n"
+              "-d [number]          :      difficulty, adjust difficulty from "
+              "1-10 any"
+              " number given superior to ten will be put at 10\n"
+              "-p [number]          :      players, adjust the number of"
+              " players (not "
+              "optional yep) max 2\n"
+              "-m [number][number]  :      adjust the number of rooms"
+              " 10x1 default\n%s", BISGREEN, RESET);
+}
+
+static int my_init(game_t *game, char **av)
+{
+    if (game->flag->help == 1) {
+        print_usage();
+        free(game->flag);
+        free(game);
+        return 0;
+    }
+    if (game->flag->game_difficulty > 10)
+        game->flag->game_difficulty = 10;
+    if (game->flag->map_size[0] > 100)
+        game->flag->map_size[0] = 100;
+    if (game->flag->map_size[1] > 100)
+        game->flag->map_size[1] = 100;
+    if (game->flag->nb_players > 2)
+        game->flag->nb_players = 2;
+    free(game->flag);
+    free(game);
+    return 0;
+}
 
 int main(int ac, char **av, char **env)
 {
@@ -22,5 +57,5 @@ int main(int ac, char **av, char **env)
         return (84);
     }
     get_arguments(game, ac, av);
-    return 0;
+    return my_init(game, av);
 }
